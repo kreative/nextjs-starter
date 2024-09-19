726580649a9413ea7ed4439033ba51907439f401
Armaan Gupta <armaan@guppy.im>
2024-09-18 19:32:08 -0500

    working on logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Container from "@/components/Container";
 import SectionDivider from "@/components/SectionDivider";
 import { Envelope } from "@phosphor-icons/react/dist/ssr";
-import { TypographicLogo } from "@/components/svgs/logos/TypographicLogo";
+import { Iconologo } from "./svgs/logos/Iconologo";
 import { MAILTO_URL } from "@/lib/constants";
 
 const SITE_URL = "https://kreativedocuvet.com";
@@ -23,23 +23,21 @@ export default function Footer() {
     <Container className="mt-24">
       <SectionDivider />
       <div className="grid grid-cols-2 gap-4 -mt-7">
-        <div className="flex flex-col items-start justify-start">
-          <TypographicLogo className="h-5 w-auto" color="rgba(0,0,0,0.4)" />
+        <div className="flex items-center justify-start space-x-3 pb-3">
+          <Iconologo className="h-5 w-auto" color="rgba(0,0,0,0.4)" />
+
+          {LINKS.map((link: any, index: number) => (
+            <div key={index}>
+              <Link
+                className="transition-colors text-[rgba(0,0,0,0.4)] hover:text-[rgba(0,0,0,0.6)]"
+                href={link.href}
+              >
+                {link.title}
+              </Link>
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col items-end justify-start pt-1">
-          <div className="flex items-center justify-end space-x-3 pb-3">
-            {LINKS.map((link: any, index: number) => (
-              <div key={index}>
-                <Link
-                  className="transition-colors text-[rgba(0,0,0,0.4)] hover:text-[rgba(0,0,0,0.6)]"
-                  href={link.href}
-                >
-                  {link.title}
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
+        <div className="flex flex-col items-end justify-start pt-1"></div>
       </div>
     </Container>
   );
